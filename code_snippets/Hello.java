28445d2dc925c79ae8010f2f8946e8eade187a1f
Luís Oliveira <luisfnqoliveira@gmail.com>
2020-05-19 12:20:24 -0400

    Adds lecture 3

---
+++ code_snippets/Hello.java
@@ -0,0 +1,15 @@
+/*
+    Author: Luis Oliveira
+    This is a simple example of a Java program
+*/
+
+public class Hello
+{
+
+  public static void main( String[] args )
+  {
+    // This is the code that will run
+    System.out.print("Hello World!");
+  }
+
+}
